3f104e979e57923e7d630ef01ca62795366e1f7a
miguel angel romero parra <mr9501459@gmail.com>
2025-03-23 13:29:45 -0500

    responsive design at text and techBar

--- src/home/components/techBar.tsx
+++ src/home/components/techBar.tsx
@@ -9,20 +9,21 @@ import GitLogo from '@assets/svg/technologies/gits.svg'
 import HtmlLogo from '@assets/svg/technologies/html.svg'
 import ReactLogo from '@assets/svg/technologies/react.svg'
 import TailwindLogo from '@assets/svg/technologies/tailwind.svg'
+
 import { useEffect, useState } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { animationProps } from '@/controllers/animations/animationProps'
 
 export default function TechBar() {
-	const [isDivVisible, setIsDivVisible] = useState<boolean>(false)
+	const [isDivVisible, setIsDivVisible] = useState(false)
 
 	// Simular carga del div con una pequeña demora
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			setIsDivVisible(true)
-			AOS.refresh() // Refrescar AOS después de cambiar el estado
-		}, animationProps.techBar.Delay) // Ajusta el tiempo según la animación del div principal
+			AOS.refresh()
+		}, animationProps.techBar.Delay)
 
 		return () => clearTimeout(timer)
 	}, [])
@@ -42,11 +43,11 @@ export default function TechBar() {
 
 	return (
 		<div
-			className={`max-w-max py-3 px-5 border-2 border-[#202126] bg-[#202126] rounded-4xl justify-self-center transition-opacity duration-500 ${
+			className={`max-w-full py-3 px-5 border-2 border-[#202126] bg-[#202126] rounded-4xl justify-self-center transition-opacity duration-500 ${
 				isDivVisible ? 'opacity-100' : 'opacity-0'
 			}`}
 		>
-			<ul className="flex gap-10">
+			<ul className="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-10">
 				{isDivVisible &&
 					techLogos.map((item, index) => (
 						<li
@@ -54,12 +55,13 @@ export default function TechBar() {
 							data-aos={animationProps.techBarElements.Animation}
 							data-aos-delay={index * 100}
 							data-aos-duration={animationProps.techBarElements.Duration}
+							className="flex items-center justify-center m-1"
 						>
 							<img
 								src={item.Logo}
 								alt={item.name}
 								title={item.name}
-								className="transition ease-in hover:scale-110 w-12"
+								className="transition ease-in hover:scale-110 w-8 sm:w-7 md:w-11"
 							/>
 						</li>
 					))}
